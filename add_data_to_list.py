fb801ef9592dd91404d1536b71f3eb05dc224ba5
Mohd Mehraj <mohammedmehrajuddin6@gmail.com>
2024-07-25 01:11:34 +0100

    add this file

---
+++ add_data_to_list.py
@@ -0,0 +1,10 @@
+fruit=["apple","mango"]
+fruit.append("mango")
+print(fruit)
+
+
+
+friuts= []
+friuts.append("merhaj")
+friuts.append("sharat")
+print(friuts)
---
+++ and_or.py
@@ -0,0 +1,6 @@
+name =("mehraj")
+age = 23
+if name== "mehrj" or age== 13:
+    print("true condition")
+else:
+    print("false condition")
